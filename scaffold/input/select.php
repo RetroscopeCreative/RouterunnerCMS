fdc4908e191b42550cf9f6e27150645254deeb2c
csibi1979 <tibor.bedekovits@retroscope.hu>
2016-01-02 13:25:04 +0100

    reworked select

--- scaffold/input/select.php
+++ scaffold/input/select.php
@@ -20,12 +20,14 @@ if (isset($field_data["help"]["panel"])) {
 <div class="form-group form-md-line-input">
 	<select name="<?=$field_name?>" id="property-<?=$field_name?>" class="input form-control <?=isset($field_data["class"]) ? $field_data["class"] : ""?>" <?=isset($field_data["multiple"]) ? $field_data["multiple"] : ""?>>
 		<?php
-		$delimiter = (isset($field_data["delimiter"]) ? $field_data["delimiter"] : ";");
-		$value = explode($delimiter, $value);
+		if (isset($field_data["delimiter"])) {
+			$delimiter = $field_data["delimiter"];
+			$value = explode($delimiter, $value);
+		}
 		$options = array();
-		if (isset($field_data["options"])) {
+		if (isset($field_data["options"]) && $field_data["options"]) {
 			$options = $field_data["options"];
-		} elseif (isset($field_data["SQL"])) {
+		} elseif (isset($field_data["SQL"]) && $field_data["SQL"]) {
 			if ($result = \db::query($field_data["SQL"])) {
 				foreach ($result as $row) {
 					$option = array_shift($row);
@@ -34,12 +36,25 @@ if (isset($field_data["help"]["panel"])) {
 				}
 			}
 		}
+		if (isset($field_data["default"])) {
+			if (!isset($options[key($field_data["default"])])) {
+				$options[key($field_data["default"])] = current($field_data["default"]);
+				ksort($options);
+			}
+			if (!$value) {
+				$value = key($field_data["default"]);
+			}
+		}
 		$is_sequential = (array_keys($options) === range(0, count($options) - 1));
 		foreach ($options as $option => $label) {
 			if ($is_sequential) {
 				$option = $label;
 			}
-			$selected = ((in_array($option, $value)) ? ' selected="selected"' : '');
+			if (isset($field_data["delimiter"])) {
+				$selected = ((in_array($option, $value)) ? ' selected="selected"' : '');
+			} else {
+				$selected = ($option == $value ? ' selected="selected"' : '');
+			}
 			echo '	<option value="' . $option . '"' . $selected . '>' . $label . '</option>'.PHP_EOL;
 		}
 		?>
